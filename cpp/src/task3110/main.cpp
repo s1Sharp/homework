c02103c091a9123081ad829baf455e37691d1e4f
Maksim "s1Sharp" Tsarkov <maksim.carkov.201300@gmail.com>
2022-11-22 21:06:39 +0300

    meta pow feature

--- cpp/src/task3110/main.cpp
+++ cpp/src/task3110/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "meta_fib.hpp"
+#include "meta_pow.hpp"
 
 int main()
 {
@@ -10,6 +11,11 @@ int main()
 	typedef meta_fib::fibon<10>::type f;
 	f a;
     //std::cout << "The full pair stack after pop: " << stack_with_pair << std::endl;
+
+ /* 
+   *    Part 2
+    */
+    std::cout << meta_p::meta_pow<5>(2.2);
 
 	return 0;
 }
--- cpp/src/task3110/meta_pow.hpp
+++ cpp/src/task3110/meta_pow.hpp
@@ -20,13 +20,13 @@ namespace meta_p {
     }
 
     template <int N, typename T>
-    typename std::enable_if<(N > 0) && (N % 2 == 1), T>::type meta_pow(T x) {
+    typename std::enable_if<(N > 1) && (N % 2 == 1), T>::type meta_pow(T x) {
         return (meta_pow< N - 1 >(x) * x);
     }
 
     template <int N, typename T>
-    typename std::enable_if<(N > 0) && (N % 2 == 0), T>::type meta_pow(T x) {
-        T p = meta_pow< (N / 2) >(x);
+    typename std::enable_if<(N > 1) && (N % 2 == 0), T>::type meta_pow(T x) {
+        T p = meta_pow< N / 2 >(x);
         return (p * p);
     }
 };
